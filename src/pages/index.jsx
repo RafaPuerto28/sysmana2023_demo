2f0adf329857de23b78aa24603281679c26e8624
RafaPuerto28 <a22punira@iesgrancapitan.org>
2023-02-02 10:53:42 +0100

    Arreglo la web

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,14 +4,12 @@ import HomePhoto from "@/components/home/HomePhoto/HomePhoto";
 import HomeTags from "@/components/home/HomeTags/HomeTags";
 import Navbar from "@/components/Navbar/Navbar";
 
-
-
 const HomePage = () =>{
     return(
         <HomePageWrapper>
             <Navbar />
             <HomePhoto />
-            {/* <HomeDescription /> */}
+            <HomeDescription />
             <HomeTags />
         </HomePageWrapper>
     );
